2523233092ca6d007b35665c08d9a2e0a020d548
ar[arnau321 <arnau123@att.net>
2021-10-16 13:50:44 -0400

    Add buttons

--- popout.js
+++ popout.js
@@ -1,3 +1,16 @@
+document.querySelector('.new-command-button').addEventListener('click', function(){
+  var newItem = `<div class="command-item">
+  <select>
+    <option value="wait">Wait</option>
+    <option value="click">Click</option>
+    <option value="enter">Enter Value</option>
+    <option value="save">Save Value</option>
+  </select>
+  <input class="value-1" placeholder="200ms"/>
+  <input class="value-2" placeholder="Optional"/>
+</div> `
+  document.querySelector('.commands-list').innerHTML+=newItem
+})
 chrome.tabs.query({currentWindow: true, active: true}), function (tabs) {
   // take in array of all tabs
   // check tab currently on
@@ -5,5 +18,5 @@ chrome.tabs.query({currentWindow: true, active: true}), function (tabs) {
 
   var obj = {}
 
-  chrome.tabs.sendMessage(activeTab.id {command: 'runCommands', data: obj})
+  chrome.tabs.sendMessage(activeTab.id, {command: 'runCommands', data: obj})
 }
