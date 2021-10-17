dc0c61fc03c83b215938b25db081d7a54d933032
ar[arnau321 <arnau123@att.net>
2021-10-16 20:17:42 -0400

    Add functionality

--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,17 @@
 chrome.runtime.onMessage.addListener((msg, sender, response) =>{
-  if(msg.name == 'runCommands'){
-    var scapeObj = msg.data
+  if(msg.command == 'runCommands'){
+    let scrapeObj = msg.data
+    console.log(scrapeObj)
   }
 })
+
+function clickEvent(selector, obj, index, endItem){
+
+}
+
+function saveEvent(selector, obj, index, endItem){
+
+}
+function enterEvent(selector, obj, index, endItem){
+  
+}
--- popout.js
+++ popout.js
@@ -1,5 +1,36 @@
+
+function createCommandObject(){
+
+  let commandsArray =[]
+
+  let commands = document.querySelectorAll('.commands-list .command-item')
+  // adds values to object and push into array
+  for(let i = 0; i < commands.length; i++){
+    let itemObj = {}
+    itemObj.type = commands[i].querySelector('select').value
+    itemObj.one = commands[i].querySelector('.value-1').value
+    itemObj.two = commands[i].querySelector('.value-2').value
+    commandsArray.push(itemObj)
+  }
+  console.log('ca', commandsArray)
+  chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
+    // take in array of all tabs
+    // check tab currently on
+    let activeTab = tabs[0]
+  
+    let obj = commandsArray
+  
+    chrome.tabs.sendMessage(activeTab.id, {command: 'runCommands', data: obj})
+  })
+}
+
+document.querySelector('.run-command-button').addEventListener('click', function(){
+  createCommandObject()
+})
+
 document.querySelector('.new-command-button').addEventListener('click', function(){
-  var newItem = `<div class="command-item">
+  // creates new fields when add command button is clicked
+  let newItem = `<div class="command-item">
   <select>
     <option value="wait">Wait</option>
     <option value="click">Click</option>
@@ -9,14 +40,15 @@ document.querySelector('.new-command-button').addEventListener('click', function
   <input class="value-1" placeholder="200ms"/>
   <input class="value-2" placeholder="Optional"/>
 </div> `
+
   document.querySelector('.commands-list').innerHTML+=newItem
 })
-chrome.tabs.query({currentWindow: true, active: true}), function (tabs) {
+chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
   // take in array of all tabs
   // check tab currently on
-  var activeTab = tabs[0]
+  let activeTab = tabs[0]
 
-  var obj = {}
+  let obj = {}
 
   chrome.tabs.sendMessage(activeTab.id, {command: 'runCommands', data: obj})
-}
+})
